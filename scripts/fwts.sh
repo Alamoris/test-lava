9b153d2f88181d5b956174d0d3ffed0c5e021de6
Volga Bogoslovskiy <v.bogoslovskiy@yadro.com>
2022-02-21 15:03:00 +0300

    Test test

---
+++ scripts/fwts.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ColinIanKing/fwts.git
+cd fwts
+apt -q update
+apt -y -q install autoconf automake libglib2.0-dev libtool libpcre3-dev flex bison dkms libfdt-dev libbsd-dev
+autoreconf -ivf
+./configure
+make
+make install
+lava-test-case hwts --shell fwts
