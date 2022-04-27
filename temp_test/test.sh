1f1c4723b099913d609fd977a68437bdb9e0e356
Volga Bogoslovskiy <v.bogoslovskiy@yadro.com>
2022-04-28 01:33:15 +0200

    Test upd

--- temp_test/test.sh
+++ temp_test/test.sh
@@ -5,7 +5,7 @@ vlan_interfaces_lines=$(lava-vland-self)
 interfaces=""
 
 # IFS=$'\n'
-for line in ${vlan_interfaces_lines//'\n'/}
+for line in ${vlan_interfaces_lines}
 do
     interfaces="${interfaces} $(echo $line | cut -d',' -f1)"
 done
