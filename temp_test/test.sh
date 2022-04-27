598345ad909f27f2b1b5ef31febc97ac3bf719a3
Volga Bogoslovskiy <v.bogoslovskiy@yadro.com>
2022-04-27 15:12:07 +0200

    Upd test

--- temp_test/test.sh
+++ temp_test/test.sh
@@ -3,13 +3,18 @@
 vlan_interfaces_lines=$(lava-vland-self)
 
 echo $vlan_interfaces_lines
-echo $vlan_interfaces_lines | while read -r a; do echo $a; done
-
 
 interfaces=()
+
+IFS='
+'
+set -f
 
 for line in ${vlan_interfaces_lines}
 do
     interfaces+=$(echo line | cut -d',' -f1)
     echo $interfaces
 done
+
+set +f
+unset IFS
