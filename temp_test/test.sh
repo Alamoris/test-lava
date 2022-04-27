b6aa720a6ade591342e3699617d54ba3ef70693f
Volga Bogoslovskiy <v.bogoslovskiy@yadro.com>
2022-04-28 01:21:55 +0200

    Test upd

--- temp_test/test.sh
+++ temp_test/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/sh -ex
 
 vlan_interfaces_lines=$(lava-vland-self)
 
@@ -7,30 +7,7 @@ interfaces=""
 IFS=$'\n'
 for line in $vlan_interfaces_lines
 do
-    interfaces+=$(echo $line | cut -d',' -f1)$','
-done
-IFS=''
-
-static_network_header="network:
-  version: 2
-  renderer: networkd
-  ethernets:"
-
-echo $static_network_header > /etc/netplan/01-netcfg.yaml
-
-
-adress_id=50
-IFS=','
-
-
-for interface in $interfaces
-do
-    static_interface="    ${interface}:
-      dhcp4: no
-      addresses: [192.168.1.${adress_id}/24]"
-    echo $static_interface >> /etc/netplan/01-netcfg.yaml
-    adress_id=$((adress_id+1))
+    interfaces="${interfaces} $(echo $line | cut -d',' -f1)"
 done
 
-netplan apply
-ifconfig
+echo $interfaces
