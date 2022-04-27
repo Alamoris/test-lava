85ab2beb7415f7e0ccbb2fd289046d698e4f3ac7
Volga Bogoslovskiy <v.bogoslovskiy@yadro.com>
2022-04-27 18:19:15 +0200

    Upd test

--- temp_test/test.sh
+++ temp_test/test.sh
@@ -10,26 +10,34 @@ do
     interfaces+=$(echo $line | cut -d',' -f1)$','
 done
 
-#static_network_header="
-#network:
-#  version: 2
-#  renderer: networkd
-#  ethernets:
-#"
+static_network_header="
+network:
+  version: 2
+  renderer: networkd
+  ethernets:"
 
-#echo $static_network_header >> /etc/netplan/01-netcfg.yaml
+echo $static_network_header >> /etc/netplan/01-netcfg.yaml
+
+
+#ip addr flush dev ${interface}
+#ip addr add 192.168.0.${adress_id}/255.255.255.0 dev ${interface}
+#ip route replace default via 192.168.0.1
 
 
 adress_id=50
 IFS=','
 for interface in $interfaces
 do
-    ip addr flush dev ${interface}
-    ip addr add 192.168.0.${adress_id}/255.255.255.0 dev ${interface}
-    ip route replace default via 192.168.0.1
+    static_interface="
+    ${interface}:
+      dhcp4: no
+      addresses: [192.168.1.${adress_id}/24]
+      gateway4: 192.168.1.1"
+    echo $static_interface >> /etc/netplan/01-netcfg.yaml
     adress_id=$(adress_id+1)
 done
 
+netplan apply
 ifconfig
 
 #static_interface="
